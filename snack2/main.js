bf0e1800df1acebc0712985370e5c0173a484e0e
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-05-30 11:42:48 +0200

    push snack 2

---
+++ snack2/main.js
@@ -0,0 +1,10 @@
+let firstWord = prompt("Scegli la prima parola");
+let secondWord = prompt("Scegli la seconda parola")
+
+if (firstWord > secondWord) {
+    console.log(secondWord, firstWord,)
+}
+
+else if (secondWord > firstWord) {
+    console.log(firstWord, secondWord)
+}
