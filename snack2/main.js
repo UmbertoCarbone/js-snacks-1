62f4fe071a5f7f6c0803a67fe455101d736ffb2a
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-05-30 11:53:11 +0200

    add else

--- snack2/main.js
+++ snack2/main.js
@@ -1,10 +1,14 @@
 let firstWord = prompt("Scegli la prima parola");
 let secondWord = prompt("Scegli la seconda parola")
 
-if (firstWord > secondWord) {
+if (firstWord > secondWord); {
     console.log(secondWord, firstWord,)
 }
 
-else if (secondWord > firstWord) {
+
+else if (secondWord > firstWord); {
+    console.log(firstWord, secondWord)
+}
+else (firstWord = secondWord); {
     console.log(firstWord, secondWord)
 }
