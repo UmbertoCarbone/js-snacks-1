67f3fb76504a0843ea6cbb5f52b4be8c9358b7bb
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-05-30 11:04:10 +0200

    Push

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let firstNumber = prompt("Scegli il primo numero");
 let secondNumber = prompt("Scegli il secondo numero")
 
+if (firstNumber > secondNumber) {
+    console.log(firstNumber)
+}
+
+else if (secondNumber > firstNumber) {
+    console.log(secondNumber)
+}
